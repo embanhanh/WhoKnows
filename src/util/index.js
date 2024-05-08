39986ac3306fdab0cb131e9c2f486a3b5dc108e1
bone204 <truongbmt4@gmail.com>
2024-05-08 13:48:56 +0700

    VCL

--- src/util/index.js
+++ src/util/index.js
@@ -1,6 +1,6 @@
 import { Platform } from "react-native";
 import io from "socket.io-client";
 
-export const BaseUrl = Platform.OS === 'android' ?  'http://localhost:3000': 'http://10.0.2.2:3000';
-export const socket = io('http://192.168.1.8:3000')
+export const BaseUrl = Platform.OS === 'android'?  'http://localhost:3000': 'http://10.0.2.2:3000';
+export const socket = io('http://172.16.1.2:3000')
 
