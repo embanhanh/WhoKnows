9f41f172fd222f86fe9cf7ba6ee7680fa63e0e51
embanhanh <dinhnhuthong59@gmail.com>
2024-05-10 22:54:07 +0700

    ...

--- src/util/index.js
+++ src/util/index.js
@@ -2,4 +2,4 @@ import { Platform } from "react-native";
 import io from "socket.io-client";
 
 export const BaseUrl = Platform.OS === 'android'?  'http://localhost:3000': 'http://10.0.2.2:3000';
-export const socket = io('http://192.168.0.103:3000')
+export const socket = io('http://192.168.43.213:3000')
